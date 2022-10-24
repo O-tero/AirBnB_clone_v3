8ba6630bab17be48b54552653b559190cc20ff66
O-tero <oteronickbrian@gmail.com>
2022-10-24 13:18:43 -0700

    Exec Files

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """Initialize Blueprint views"""
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")   
+
 from api.v1.views.users import *
 from api.v1.views.states import *
 from api.v1.views.places_reviews import *
@@ -10,4 +13,4 @@ from api.v1.views.amenities import *
 from api.v1.views.index import *
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
