2a5242deba89bdf993d33b4366349de9061cc614
O-tero <oteronickbrian@gmail.com>
2022-10-24 17:14:52 +0300

    AirBnB clone V3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
-"""Index view"""
+"""This module implement a rule that returns the status of the application"""
+from flask import jsonify
+import models
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
-from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-stats = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
-
-
-@app_views.route('/status', strict_slashes=False)
-def status_route():
-    """Status of the web server"""
+@app_views.route("/status", strict_slashes=False)
+def view_status():
+    """View function that return a json message"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def stats_route():
-    """Stats"""
-    return_dict = {}
-    for key, value in stats.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
-
-if __name__ == "__main__":
-    pass
+@app_views.route("/stats", strict_slashes=False)
+def view_stats():
+    """Veiw function that retrieves the number of each object by type"""
+    return jsonify({
+        "amenities": models.storage.count(Amenity),
+        "cities": models.storage.count(City),
+        "places": models.storage.count(Place),
+        "reviews": models.storage.count(Review),
+        "states": models.storage.count(State),
+        "users": models.storage.count(User)
+    })
