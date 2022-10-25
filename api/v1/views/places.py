7569c32046c85767f1eedb1aa91f8c93a6c15fad
O-tero <oteronickbrian@gmail.com>
2022-10-25 05:25:14 +0300

    AirBnB clone V3

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,137 +1,78 @@
-#!/usr/bin/python3
-"""
-Places view for RESTFull Api V1
-"""
+#!/usr/bin/python
+""" holds class Place"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
+if models.storage_t == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id', onupdate='CASCADE',
+                                            ondelete='CASCADE'),
+                                 primary_key=True),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id', onupdate='CASCADE',
+                                            ondelete='CASCADE'),
+                                 primary_key=True))
 
 
-from flask import jsonify, abort, request
-from api.v1.views import app_views
-from models.place import Place
-from models.city import City
-from models.user import User
-from models import storage
-
-
-@app_views.route(
-    '/cities/<city_id>/places',
-    strict_slashes=False,
-    methods=['GET']
-)
-def get_all_places_by_city(city_id):
-    """
-    get_all_places_by_city method that retrieves all the objects of places
-    class by city
-    Arguments:
-        city_id = Id of city
-    Return:
-        Json Object
-    """
-    city_by_id = storage.get(City, city_id)
-    if city_by_id is None:
-        abort(404)
-    return jsonify(
-        [
-            place.to_dict() for place in city_by_id.places
-        ]
-    )
-
-
-@app_views.route(
-    '/places/<place_id>/',
-    strict_slashes=False,
-    methods=['GET'])
-def get_place_by_id(place_id):
-    """
-    get_place_by_id method that return place object by id
-    Arguments:
-        place_id: Id of place
-    Return:
-        Json Object
-    """
-    place_by_id = storage.get(Place, place_id)
-    if place_by_id is None:
-        abort(404)
-    return jsonify(place_by_id.to_dict())
-
-
-@app_views.route(
-    '/places/<place_id>/',
-    strict_slashes=False,
-    methods=['DELETE'])
-def delete_place_by_id(place_id):
-    """
-    delete_place_by_id method that delete place object by id
-    Arguments:
-        place_id: Id of place
-    Return:
-        empty dictionary
-    """
-    place_by_id = storage.get(Place, place_id)
-    if place_by_id is None:
-        abort(404)
+class Place(BaseModel, Base):
+    """Representation of Place """
+    if models.storage_t == 'db':
+        __tablename__ = 'places'
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        reviews = relationship("Review", backref="place")
+        amenities = relationship("Amenity", secondary="place_amenity",
+                                 backref="place_amenities",
+                                 viewonly=False)
     else:
-        storage.delete(place_by_id)
-        storage.save()
-        return jsonify({}), 200
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
 
+    def __init__(self, *args, **kwargs):
+        """initializes Place"""
+        super().__init__(*args, **kwargs)
 
-@app_views.route(
-    "/cities/<city_id>/places",
-    strict_slashes=False,
-    methods=['POST']
-)
-def post_create_place(city_id):
-    """
-    post_create_place method that create a new place object
-    Arguments:
-        city_id: Id of city to the new place
-    Return:
-        Json Object with the new place
-    """
-    city_by_id = storage.get(City, city_id)
-    if city_by_id is None:
-        abort(404)
-    new_requ = request.get_json()
-    if not new_requ:
-        abort(400, 'Not a JSON')
-    if 'name' not in new_requ:
-        abort(400, 'Missing name')
-    if 'user_id' not in new_requ:
-        abort(400, 'Missing user_id')
+    if models.storage_t != 'db':
+        @property
+        def reviews(self):
+            """getter attribute returns the list of Review instances"""
+            from models.review import Review
+            review_list = []
+            all_reviews = models.storage.all(Review)
+            for review in all_reviews.values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
 
-    user_id = new_requ['user_id']
-    user = storage.get(User, user_id)
-
-    if user is None:
-        abort(404)
-
-    new_place = Place(**request.get_json())
-    new_place.city_id = city_id
-    new_place.user_id = user.id
-    storage.new(new_place)
-    storage.save()
-    return jsonify(new_place.to_dict()), 201
-
-
-@app_views.route(
-    '/places/<place_id>',
-    strict_slashes=False,
-    methods=['PUT'])
-def put_update_place(place_id):
-    """
-    put_update_place method that updating a place by id
-    Arguments:
-        place_id: Id of place
-    Return:
-        Json Object
-    """
-    new_requ = request.get_json()
-    if not new_requ:
-        abort(400, 'Not a JSON')
-    place_by_id = storage.get(Place, place_id)
-    if place_by_id:
-        for attr, value in request.get_json().items():
-            setattr(place_by_id, attr, value)
-        storage.save()
-        return jsonify(place_by_id.to_dict()), 200
-    abort(404)
+        @property
+        def amenities(self):
+            """getter attribute returns the list of Amenity instances"""
+            from models.amenity import Amenity
+            amenity_list = []
+            all_amenities = models.storage.all(Amenity)
+            for amenity in all_amenities.values():
+                if amenity.place_id == self.id:
+                    amenity_list.append(amenity)
+            return amenity_list
