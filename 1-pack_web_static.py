4dc68e18b3a1d53572ce8267af69836751901c9d
O-tero <oteronickbrian@gmail.com>
2022-10-25 23:43:01 +0300

    Exec files

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """
 Fabric script that generates a tgz archive from the contents of the web_static
@@ -18,5 +19,5 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except:
+    except BaseException:
         return None
